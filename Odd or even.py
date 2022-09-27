d750b5549fc692c9ce7667d18a37b35e30970ea0
Marcos Fiandrino <marcosfiandrino@gmail.com>
2022-09-27 17:04:26 -0300

    Added the extra exercise asking for multiple of 4, changed description of function

--- Odd or even.py
+++ Odd or even.py
@@ -4,13 +4,18 @@ user_number = int(input("Give me a number: "))
 
 """We do a function to check if the modulus operation or remainder of dividing by 2 is:
 if the modulus is 0, this means the int is even, if the modulus is 1, the number is odd"""
+"""We add as the extra says to check if the number is multiple by 4, adding an extra message
+in case it is"""
 
 def odd_or_even(input_number):
-    input_number = input_number % 2
-    if input_number == 0:
-        print("Your number is an even number")
+    if input_number % 2 == 0 and input_number % 4 != 0:
+        print("Your number " + str(input_number) + " is an even number")
+    elif input_number % 2 == 0 and input_number % 4 == 0:
+        print("Your number " + str(input_number) + " is multiple of 4 and even at the same time")
     else:
-        print("Your number is an odd number")
+        print("Your number " + str(input_number) + " is an odd number")
+
+
 
 """We apply the function to the user number"""
 
