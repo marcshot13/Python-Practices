9883230fd21a2786a1fef257dbe1a974581201c6
Marcos Fiandrino <marcosfiandrino@gmail.com>
2022-09-27 17:17:48 -0300

    1st extra of the exercise and description added

--- List less than ten.py
+++ List less than ten.py
@@ -2,13 +2,18 @@
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
+"""Made an empty list to store the values of the lesser than 5 items in the list"""
+
+z = []
+
 """Made a for loop to check wether every item in the list one by one was less than five"""
 
 def point_less_than_five(list):
     for i in list:
         if i < 5:
-            print(i)
+            z.append(i)
 
-"""Ran the function"""
+"""Ran the function and printed the result"""
 
 point_less_than_five(a)
+print(z)
