ceac6e808f08055a0eecbc42e4bf28b5aba51c29
Marcos Fiandrino <marcosfiandrino@gmail.com>
2022-09-27 17:14:04 -0300

    Added lacking commentary

--- List less than ten.py
+++ List less than ten.py
@@ -2,9 +2,13 @@
 
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
 
+"""Made a for loop to check wether every item in the list one by one was less than five"""
+
 def point_less_than_five(list):
     for i in list:
         if i < 5:
             print(i)
+
+"""Ran the function"""
 
 point_less_than_five(a)
